b9d9bf032741b2763a1a7bf50a7263e698b5664c
efrein <eiguedez@misena.edu.co>
2022-09-22 11:45:15 -0500

    Avance 22 sep
    Carrito, Borrar y Carrito

--- webDjango/appProductos/urls.py
+++ webDjango/appProductos/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('producto/', views.verProductos, name="productos"),
     path('producto/<str:id>', views.verProductos, name="producto"),
     path('carrito/<str:id>', views.agregar, name="carrito"),
+    path('vercarrito/', views.verCarrito, name="ver_carrito"),
+    path('eliminar/<str:id>', views.eliminarItemCarrito, name="eliminar"),    
 ]
 
--- webDjango/appProductos/views.py
+++ webDjango/appProductos/views.py
@@ -26,9 +26,9 @@ def agregar (request, id= NULL):
     id = int(id)
     user = request.user
     regProducto = Producto.objects.get(id=id)
-    existe= Carrito.objects.filter(producto = regProducto, estado ='carrito').exists()
+    existe= Carrito.objects.filter(cliente=user, producto = regProducto, estado ='carrito').exists()
     if existe:
-        regCarrito = Carrito.objects.get(producto = regProducto, estado ='carrito')
+        regCarrito = Carrito.objects.get(cliente=user, producto = regProducto, estado ='carrito')
         regCarrito.cantidad += 1
         regCarrito.save()
     else:
@@ -40,3 +40,43 @@ def agregar (request, id= NULL):
             'productos': listaProductos,
         }
     return render(request, 'productos/productos.html', context)
+
+
+def verCarrito(request):
+    regUser = request.user
+    carrito = Carrito.objects.filter(cliente=regUser, estado='carrito')
+    # Recorrer elementos del carrito para calcular totales
+
+    listaCarrito = []
+    total = 0
+    for prod in carrito:
+        unProducto={
+			'cantidad': prod.cantidad,
+			'icono': prod.producto.icono,
+			'nombre': prod.producto.nombre,
+			'valor': prod.producto.precio,
+			'unidad': prod.producto.unidad	,
+			'total': int(prod.cantidad)	* int(prod.producto.precio),
+			'id': prod.id,
+	    }
+        listaCarrito.append(unProducto) 
+        total += unProducto['total']
+
+    #ensamblar datos para la plantilla
+    context = {
+        'carrito':  listaCarrito,
+        'subtotal': total,
+        'iva': total * 0.19,
+        'envio': 8000,
+        'total': total * 1.19 + 8000,
+    }
+    return render(request, 'productos/carrito.html', context)
+
+def eliminarItemCarrito(request, id):
+    #Consular
+    getCarrito = Carrito.objects.get(id=id)
+    getCarrito.estado = 'cancelado'
+    #Guardar en BD
+    getCarrito.save()
+    #Desplazar el carrito
+    return verCarrito(request)
